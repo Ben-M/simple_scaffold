69ca0feb611d663b07fae420e24318ec6b64f91f
Ben Maraney <ben.maraney@klarna.com>
2013-12-29 11:16:23 +0200

    Fix indentation

--- simple_scaffold.rb
+++ simple_scaffold.rb
@@ -1,60 +1,60 @@
 def generate_controller(update_method, params, include_white_list_code=false)
   strong_params_method=white_list_code if include_white_list_code
   controller_code = <<-CODE
-  <% if namespaced? -%>
-  require_dependency "<%= namespaced_file_path %>/application_controller"
+<% if namespaced? -%>
+require_dependency "<%= namespaced_file_path %>/application_controller"
 
-  <% end -%>
-  <% module_namespacing do -%>
-  class <%= controller_class_name %>Controller < ApplicationController
+<% end -%>
+<% module_namespacing do -%>
+class <%= controller_class_name %>Controller < ApplicationController
 
-    def index
-      @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
-      render 'index'
-    end
-
-    def show
-      @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-      render 'show'
-    end
-
-    def new
-      @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
-      render 'new'
-    end
-
-    def edit
-      @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-      render 'edit'
-    end
-
-    def create
-      @<%= singular_table_name %> = <%= orm_class.build(class_name, "#{params}") %>
-
-      if @<%= orm_instance.save %>
-        redirect_to @<%= singular_table_name %>, notice: <%= "'\#{human_name} was successfully created.'" %>
-      else
-        render action: 'new'
-      end
-    end
-
-    def update
-      @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-      if @<%= orm_instance.#{update_method}("#{params}") %>
-        redirect_to @<%= singular_table_name %>, notice: <%= "'\#{human_name} was successfully updated.'" %>
-      else
-        render action: 'edit'
-      end
-    end
-
-    def destroy
-      @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-      @<%= orm_instance.destroy %>
-      redirect_to <%= index_helper %>_url, notice: <%= "'\#{human_name} was successfully destroyed.'" %>
-    end
-  #{strong_params_method}
+  def index
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+    render 'index'
   end
-  <% end -%>
+
+  def show
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    render 'show'
+  end
+
+  def new
+    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    render 'new'
+  end
+
+  def edit
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    render 'edit'
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "#{params}") %>
+
+    if @<%= orm_instance.save %>
+      redirect_to @<%= singular_table_name %>, notice: <%= "'\#{human_name} was successfully created.'" %>
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    if @<%= orm_instance.#{update_method}("#{params}") %>
+      redirect_to @<%= singular_table_name %>, notice: <%= "'\#{human_name} was successfully updated.'" %>
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    @<%= orm_instance.destroy %>
+    redirect_to <%= index_helper %>_url, notice: <%= "'\#{human_name} was successfully destroyed.'" %>
+  end
+#{strong_params_method}
+end
+<% end -%>
   CODE
   file 'lib/templates/rails/scaffold_controller/controller.rb', controller_code
 end
@@ -62,15 +62,15 @@ end
 def white_list_code
   <<-CODE
 
-    private
-    # Only allow a trusted parameter "white list" through.
-    def <%= "\#{singular_table_name}_params" %>
-      <%- if attributes_names.empty? -%>
-      params[<%= ":\#{singular_table_name}" %>]
-      <%- else -%>
-      params.require(<%= ":\#{singular_table_name}" %>).permit(<%= attributes_names.map { |name| ":\#{name}" }.join(', ') %>)
-      <%- end -%>
-    end
+  private
+  # Only allow a trusted parameter "white list" through.
+  def <%= "\#{singular_table_name}_params" %>
+    <%- if attributes_names.empty? -%>
+    params[<%= ":\#{singular_table_name}" %>]
+    <%- else -%>
+    params.require(<%= ":\#{singular_table_name}" %>).permit(<%= attributes_names.map { |name| ":\#{name}" }.join(', ') %>)
+    <%- end -%>
+  end
   CODE
 end
 
